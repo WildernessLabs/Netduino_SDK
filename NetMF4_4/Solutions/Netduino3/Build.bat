b996cf0e07ba5b4eb040d236efc6562ea1471729
NevynUK <mark@thepcsite.co.uk>
2018-07-04 09:09:55 +0100

    Call to batch file to prevent process stopping on Windows 10

--- NetMF4_4/Solutions/Netduino3/Build.bat
+++ NetMF4_4/Solutions/Netduino3/Build.bat
@@ -3,7 +3,7 @@ SET Base=C:\GitHub\Netduino_SDK\NetMF4_4
 SET Proj=Netduino3
 cd %Base%
 RMDIR %Base%\BuildOutput /S /Q
-setenv_mdk 5.05
+call setenv_mdk 5.05
 cd %Base%\Solutions\%Proj%
 msbuild /flp:ErrorsOnly;LogFile=errors.log /v:m
 move %Base%\BuildOutput\THUMB2FP\MDK5.05\le\FLASH\release\%Proj%\bin\Tinybooter.hex %Base%\BuildOutput\THUMB2FP\MDK5.05\le\FLASH\release\%Proj%\bin\tinyclr.hex\TinyBooter.S19
