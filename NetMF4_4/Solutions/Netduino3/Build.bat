7fbdea6263770f80f0b96b741a28151531cac949
NevynUK <mark@thepcsite.co.uk>
2018-08-24 17:53:52 +0100

    Renamed some of the files and used environment variables in path

--- NetMF4_4/Solutions/Netduino3/Build.bat
+++ NetMF4_4/Solutions/Netduino3/Build.bat
@@ -1,10 +1,14 @@
 @ECHO ON
-SET Base=C:\GitHub\Netduino_SDK\NetMF4_4
 SET Proj=Netduino3
-cd %Base%
-RMDIR %Base%\BuildOutput /S /Q
+cd %SPOCLIENT%
+RMDIR %SPOCLIENT%\BuildOutput /S /Q
 call setenv_mdk 5.05
-cd %Base%\Solutions\%Proj%
+cd %SPOCLIENT%\Solutions\%Proj%
 msbuild /flp:ErrorsOnly;LogFile=errors.log /v:m
-move %Base%\BuildOutput\THUMB2FP\MDK5.05\le\FLASH\release\%Proj%\bin\Tinybooter.hex %Base%\BuildOutput\THUMB2FP\MDK5.05\le\FLASH\release\%Proj%\bin\tinyclr.hex\TinyBooter.S19
-%SystemRoot%\explorer.exe %Base%\BuildOutput\THUMB2FP\MDK5.05\le\FLASH\release\%Proj%\bin 
+pushd .
+cd %SPOCLIENT%\BuildOutput\THUMB2FP\MDK5.05\le\FLASH\release\%Proj%\bin\tinyclr.hex
+move ..\Tinybooter.hex .
+ren ER_FLASH ER_FLASH.hex
+ren ER_CONFIG ER_CONFIG.hex
+%SystemRoot%\explorer.exe %SPOCLIENT%\BuildOutput\THUMB2FP\%COMPILER_TOOL_VERSION%\le\%FLAVOR_MEMORY%\%FLAVOR_DAT%\%Proj%\bin\tinyclr.hex
+popd
